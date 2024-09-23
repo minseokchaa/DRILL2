1b39a882a5c64c5e2e357a887ec2aebe26b4dcdb
minseokcha@laptop <alstjr0526@gmail.com>
2024-09-23 15:09:53 +0900

    Drill 2 제출 (2021180040 차민석)

---
+++ Drill2_2021180040_차민석.py
@@ -0,0 +1,59 @@
+from pico2d import*
+import math
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x=400
+y=90
+r=0
+a=50
+while(x < 600):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,90)
+    x=x+2
+    delay(0.01)
+
+
+while(x > 400):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    x=x-2
+    y=y+3.46
+    delay(0.01)
+
+    
+while(x > 200):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    x=x-2
+    y=y-3.46
+    delay(0.01)
+    
+while(x < 400):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,90)
+    x=x+2
+    delay(0.01)    
+    
+while(r < 360):
+    x=400-a*math.cos*r
+    y=90+a+a*sin*r
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400-a*cos,90)
+    r=r+1
+    delay(0.01)  
+
+
+
+
+    
+
+
+
